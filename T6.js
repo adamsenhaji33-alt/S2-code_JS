d82a43942970b23cf17da5cb76835d230dbc6a03
adamsenhaji33-alt <adamsenhaji33@gmail.com>
2026-09-21 14:29:42 +0100

    T6

--- T6.js
+++ T6.js
@@ -1,3 +1,72 @@
-console.log(10 < 20);
-console.log("bla bla");
-console.log("hahahahah");
+// Checking a comparison
+
+console.log(6<10);
+
+// Test several comparisons
+
+console.log(60 === 60);
+console.log(30 !== 20);
+console.log(5 >= 5);
+console.log(10 <= 19);
+console.log(10 < 5);
+console.log(10 > 5);
+
+// Using a variable
+
+let mohamde_age = 39;
+console.log(mohamde_age <= 100);
+
+//Store the result
+
+let age1 = 20;
+let estMajeur = age1 >=18;
+console.log(estMajeur);
+
+// Use&&
+
+let age2 = 20;
+let lmuf9a = true;
+let access = age2 >= 18 && lmuf9a === true;
+console.log(access);
+ 
+// Use|| 
+
+let membre = false;
+let invitasion = true;
+let Access = membre === true || invitasion === true;
+console.log(Access);
+
+// Use! 
+
+let posible = false;
+console.log(!posible);
+
+// Exercise — Predicting the result
+console.log(5>2);
+console.log(5<2);
+console.log(10 === 10);
+console.log(10 !== 10);
+console.log(8 >= 8);
+console.log(3 <= 2);
+
+let age3 = 20;
+let tste = true;
+let access2 = age3 >= 18 && tste === true;
+console.log(access2);
+ 
+let Access3 = age3 >= 18 && tste === false;
+console.log(Access3);
+
+let age4 = 20;
+let membre2 = true;
+let access4 = age4 >= 30 || membre2 === true;
+console.log(access4);
+
+let access5 = (age3 < 18);
+console.log(!access5);
+
+// exercise - 2 
+
+let age5 = 20;
+let estMajeur2 = age5 >= 18;
+console.log(estMajeur2);

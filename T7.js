41f31397c3276a586052f4eba9d5b65fc05c403f
adamsenhaji33-alt <adamsenhaji33@gmail.com>
2026-09-22 13:00:31 +0100

    T7-Construire des conditions

---
+++ T7.js
@@ -0,0 +1,13 @@
+
+
+
+let note = 20;
+if (note >= 16 ) {
+    console.log("You are a very good .");
+} else if (note >= 10 && note < 16) {
+    console.log("You are a passed .");
+}else {
+    console.log("You are not a failed.");
+}
+
+

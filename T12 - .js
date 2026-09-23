3dae45fe999651c277f15391703a63889944ddcf
adam senhaji <adamsenhaji07@gmaile.com>
2026-09-23 14:50:07 +0100

    T12-Installer et configurer Git sur son ordinateur

---
+++ T12 - .js
@@ -0,0 +1,13 @@
+$ git --version
+
+git version 2.43.0 ✓ installé
+
+👤 Configuration de l'identité
+
+$ git config --global user.name
+
+adam senhaji ✓ configuré
+
+$ git config --global user.email
+
+adamsenhaji07@gmaile.com ✓ configuré
